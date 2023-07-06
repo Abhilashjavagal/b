ea64c4fb2fe400b57a10cba809f28e0be0148412
Abhilashjavagal <abhilash.javagal@lucidatechnologies.com>
2023-07-06 12:51:12 +0530

    Booking Design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import LoginPage from './Components/LoginPage';
+import SignupPage from './Components/SignupPage';
+import Sidebar from './Components/Sidebar';
+import AddBooking from './Components/Bookings/AddBooking';
+import { Route, Routes } from "react-router-dom";
 import './App.css';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Room from './Components/Rooms/Room';
+import Booking from './Components/Bookings/Booking';
+import AddRoom from './Components/Rooms/AddRoom';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/room" element={<Room />} />
+        <Route path="/addbooking" element={<AddBooking />} />
+        <Route path="/addrooms" element={<AddRoom />} />
+        <Route path="/booking" element={<Booking/>} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+</div>
   );
-}
+};
 
 export default App;
